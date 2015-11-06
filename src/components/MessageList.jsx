9ca005d6d0c07490b57cd9e153ebc7dd87eb07ad
Josh Newman <j.newman@apple.com>
2015-11-05 21:49:41 -0800

    Hooked into Firebase and real time message support

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx';
 import mui from 'material-ui';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 var {Card, List} = mui;
 
@@ -8,17 +10,32 @@ class MessageList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [
-        'Hello there how are you?',
-        'I am fine, and you?'
-      ]
+      messages: []
     };
+
+    this.firebaseRef = new Firebase('https://react-stack-jn.firebaseio.com/messages');
+    this.firebaseRef.on("child_added", (msg)=> {
+      if(this.state.messages[msg.key()]) {
+        return;
+      }
+
+      let msgVal = msg.val();
+      msgVal.key = msg.key();
+      this.state.messages[msgVal.key] = msgVal;
+      this.setState({messages: this.state.messages});
+    });
+
+    this.firebaseRef.on("child_removed", (msg) => {
+      var key = msg.key();
+      delete this.state.messages[key];
+      this.setState({messages: this.state.messages});
+    })
   }
 
   render() {
-    var messageNodes = this.state.messages.map((message) => {
+    var messageNodes = _.values(this.state.messages).map((message) => {
       return (
-        <Message message={message}/>
+        <Message message={message.message}/>
       );
     });
 
